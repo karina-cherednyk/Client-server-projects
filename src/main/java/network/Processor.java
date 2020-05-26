dbe00e141e7df89775314494e2917f7f4b1d32ce
Cherednyk Karina <karina0cher@gmail.com>
2020-05-26 23:17:55 +0300

    OK, FAIL added

--- src/main/java/network/Processor.java
+++ src/main/java/network/Processor.java
@@ -7,17 +7,33 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
-public class Processor implements Runnable{
+public class Processor {
     private static ExecutorService threadPool = Executors.newFixedThreadPool(6);
-    private Message message;
+
     private static AtomicInteger counter = new AtomicInteger(0);
 
-    private Processor(Message message) {
-        this.message = message;
-    }
 
     public static void process(Message message){
-        threadPool.submit( new Processor(message) );
+        int c = counter.getAndIncrement();
+        Package pack = new Package((byte)c,c,c,c,"OK");
+        process(pack);
+    }
+    private static void process(Package pack){
+        threadPool.submit(()->{
+
+            try {
+                byte[] bytePack = PackageProcessor.encode(pack);
+                System.out.println("Sending response "+pack.getBmsq()+" to "+InetAddress.getLocalHost());
+                TCPNetwork.getInstance().sendMessage(bytePack, InetAddress.getLocalHost());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } );
+    }
+    public static void processFail() {
+        int c = counter.getAndIncrement();
+        Package pack = new Package((byte)c,c,c,c,"FAIL");
+        process(pack);
     }
 
     public static  void shutdown(){
@@ -33,18 +49,5 @@ public class Processor implements Runnable{
         if(threadPool.isTerminated()) threadPool = Executors.newFixedThreadPool(6);
     }
 
-    @Override
-    public void run() {
-        System.out.println("Message "+message+" was received");
-        int c = counter.getAndIncrement();
-        Package pack = new Package((byte)c,c,c,c,"OK");
-        try {
-            byte[] bytePack = PackageProcessor.encode(pack);
-            System.out.println("Sending response "+pack.getBmsq()+" to "+InetAddress.getLocalHost());
-            TCPNetwork.getInstance().sendMessage(bytePack, InetAddress.getLocalHost());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-    }
 }
